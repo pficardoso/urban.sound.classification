398e5ab3fa1f466db17c65fa8d49218962389b90
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2020-08-22 18:20:04 +0100

    Module SoundsDatabase.py created

---
+++ modules/SoundsDatabase.py
@@ -0,0 +1,23 @@
+from os import path
+import pandas as pd
+
+class SoundsDatabase():
+
+    def __init__(self, csv_path, audiofiles_dir_path):
+        self.csv_path  = path.abspath(csv_path)
+        self.audiofiles_dir = path.abspath(audiofiles_dir_path)
+        self.data_df = pd.read_csv(csv_path).set_index("ID")
+
+
+    def __len__(self):
+        return self.data_df.shape[0]
+
+    def __getitem__(self, item):
+        audio_name = "".join([str(self.data_df.iloc[item].name), ".wav"])
+        return "/".join([self.audiofiles_dir, audio_name])
+
+    def get_labels(self):
+        return list(self.data_df.Class)
+
+    def get_ids(self):
+        return list(self.data_df.index.values)
